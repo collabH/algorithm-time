787b4e2d62a59b4c76245b0f4d13794d799b0dc7
Shiming Huang <h1261109615@qq.com>
2020-10-22 10:50:21 +0800

    feat: 背包问题变体，求分割等和子集

--- src/main/java/org/algorithm/dp/LeetCode416.java
+++ src/main/java/org/algorithm/dp/LeetCode416.java
@@ -7,7 +7,51 @@ package org.algorithm.dp;
  * @date: 2020/10/21 3:26 下午
  */
 public class LeetCode416 {
+    /**
+     * 能够得到一种背包问题的变体条件
+     * sum(nums)/2=
+     *
+     * @param nums
+     * @return
+     */
     public boolean canPartition(int[] nums) {
-        return false;
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxNum = Math.max(maxNum, num);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        boolean[][] dp = new boolean[n][target + 1];
+        // base case dp[i][0]=true dp[0][j]=false
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = false;
+        // 做选择
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            // 做选择
+            for (int j = 1; j <= target; j++) {
+                // 如果重量大于该num
+                if (j >= num) {
+                    // 要么不放入背包，要么放入背包
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    // 重量小于num，不放入背包
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
     }
 }
