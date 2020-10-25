9513379492414a92f1a01b9e9ad36eb434bc8541
Shiming Huang <h1261109615@qq.com>
2020-10-25 20:59:33 +0800

    feat: 添加链表元素的移除

---
+++ src/main/java/org/datastructure/linkedlist/LeetCode203.java
@@ -0,0 +1,35 @@
+package org.datastructure.linkedlist;
+
+/**
+ * @fileName: LeetCode203.java
+ * @description: 移除链表元素
+ * @author: by echo huang
+ * @date: 2020/10/25 4:09 下午
+ */
+public class LeetCode203 {
+    public ListNode removeElements(ListNode head, int val) {
+        // 虚拟头节点
+        ListNode pred = new ListNode(-1);
+        pred.next = head;
+        ListNode pre = pred, curr = head;
+        while (curr != null) {
+            if (curr.val == val) {
+                pre.next = curr.next;
+            } else {
+                pre =curr;
+            }
+            curr = curr.next;
+        }
+        return pred.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
+
