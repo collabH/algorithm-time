ef0e85ac112a7e4e4bfd0e0dba52c8dd23861005
Shiming Huang <h1261109615@qq.com>
2020-10-28 09:52:50 +0800

    feat: 最递增子序列问题

--- src/main/java/org/algorithm/dp/LeetCode300.java
+++ src/main/java/org/algorithm/dp/LeetCode300.java
@@ -2,18 +2,22 @@ package org.algorithm.dp;
 
 /**
  * @fileName: LeetCode673.java
- * @description: 最长递增子序列的个数
+ * @description: 最长上升子序列
  * @author: by echo huang
  * @date: 2020/10/27 5:42 下午
  */
-public class LeetCode673 {
+public class LeetCode300 {
     public static void main(String[] args) {
-        int [] nums={1,3,5,4,7};
-        LeetCode673 leetCode673 = new LeetCode673();
-        System.out.println(leetCode673.findNumberOfLIS(nums));
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        LeetCode300 leetCode300 = new LeetCode300();
+        System.out.println(leetCode300.lengthOfLIS(nums));
     }
-    public int findNumberOfLIS(int[] nums) {
+
+    public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
         int[] dp = new int[n];
         // base case
         for (int i = 0; i < n; i++) {
@@ -22,22 +26,15 @@ public class LeetCode673 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
-                    // dp[i] , dp[j]上一个小于dp[i]的值加1
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
+
         int max = 0;
         for (int i : dp) {
             max = Math.max(i, max);
         }
-        int count=0;
-        for (int i : dp) {
-            if (max==i){
-                count++;
-            }
-        }
-
-        return count;
+        return max;
     }
 }
