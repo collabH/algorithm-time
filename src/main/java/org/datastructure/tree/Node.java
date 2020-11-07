904369d87354451c3f413ab8a3fe5f1197fd9ed8
Shiming Huang <h1261109615@qq.com>
2020-11-07 23:22:31 +0800

    feat: 红黑树

--- src/main/java/org/datastructure/tree/Node.java
+++ src/main/java/org/datastructure/tree/Node.java
@@ -10,11 +10,16 @@ public class Node<E> {
     public E e;
     public Node<E> left, right;
     public int height;
+    public Color color;
 
     public Node(E e) {
         this.e = e;
         height = 1;
         left = null;
         right = null;
+        this.color=Color.BLACK;
+    }
+    public enum Color{
+        RED,BLACK
     }
 }
