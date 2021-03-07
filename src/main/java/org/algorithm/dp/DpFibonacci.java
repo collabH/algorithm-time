423fe2753d669a46422e97ebe5482907f99aea56
Shiming Huang <h1261109615@qq.com>
2021-03-07 22:00:14 +0800

    feat:[dp] review dp

--- src/main/java/org/algorithm/dp/DpFibonacci.java
+++ src/main/java/org/algorithm/dp/DpFibonacci.java
@@ -68,6 +68,21 @@ public class DpFibonacci {
         return dpTable.get(n);
     }
 
+    static int dpTable2(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+
+
+
     /**
      * 状态压缩
      *
